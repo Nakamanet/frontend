9b1a9c9a084d13d669539eae6a68bff3b9fdcd2d
Gauthier Watelain <gauthier.watelain@epitech.eu>
2026-03-20 10:57:11 +0100

    fix page profil

--- src/app/profile/components/Activity.tsx
+++ src/app/profile/components/Activity.tsx
@@ -4,53 +4,13 @@ import PostCards from "../../components/PostCards";
 import { User } from "../../types/auth";
 import { useState } from "react";
 import { Flame, ThumbsUp, Bookmark, Trash } from "lucide-react";
+import { Post } from "../../types/post";
 
 export default function Activity({ user }: { user: User }) {
     const [filter, setFilter] = useState('all');
 
     // A modifier quand les routes seront intégrées
-    const posts = [
-        {
-            id: 1,
-            avatar_url: "/logo.png",
-            username: user.username,
-            content: "Bonjour, comment ça va ?",
-            created_at: "2026-03-10T12:00:00.000000Z",
-            updated_at: "2026-03-10T12:00:00.000000Z",
-        },
-        {
-            id: 2,
-            avatar_url: "/logo.png",
-            username: user.username,
-            content: "Aujourd'hui j'ai fini la nouvelle saison de #SNK et c'etait vraiment lourd !",
-            created_at: "2026-03-06T12:00:00.000000Z",
-            updated_at: "2026-03-06T12:00:00.000000Z",
-        },
-        {
-            id: 3,
-            avatar_url: "/logo.png",
-            username: user.username,
-            content: "Mais qu'es ce qui lui arrive a Erenn ? Il a pété son crane ou quoi ?",
-            created_at: "2026-03-01T12:00:00.000000Z",
-            updated_at: "2026-03-01T12:00:00.000000Z",
-        },
-        {
-            id: 4,
-            avatar_url: "/logo.png",
-            username: user.username,
-            content: "Woaw le Gear 5 c'est quoi cette dinguerie ?!",
-            created_at: "2026-02-25T12:00:00.000000Z",
-            updated_at: "2026-02-25T12:00:00.000000Z",
-        },
-        {
-            id: 5,
-            avatar_url: "/logo.png",
-            username: user.username,
-            content: "Je viens de commencer la nouvelle saison de #OnePiece et c'est vraiment une bombe !",
-            created_at: "2026-02-18T12:00:00.000000Z",
-            updated_at: "2026-02-18T12:00:00.000000Z",
-        },
-    ];
+    const [posts, setPosts] = useState<Post[]>([]);
 
     return (
         <div className="flex flex-col gap-8 p-7 ">
@@ -110,9 +70,15 @@ export default function Activity({ user }: { user: User }) {
                     </button>
                 </div>
             </div>
-            {posts.map((post) => (
-                <PostCards key={post.id} post={post} />
-            ))}
+            {posts && posts.length > 0 ? (
+                <div>
+                    {posts.map((post) => (
+                        <PostCards key={post.id} post={post} />
+                    ))}
+                </div>
+            ) : (
+                <p>Vous n&apos;avez pas encore de posts</p>
+            )}
         </div>
     );
 }
