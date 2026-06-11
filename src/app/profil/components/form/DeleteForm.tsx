a4ee7a3b99856eb4b528318037c29a7d247892c3
Gauthier Watelain <gauthier.watelain@epitech.eu>
2026-06-11 17:07:35 +0200

    [FIX] Remove commented-out logout call in DeleteForm component

--- src/app/profil/components/form/DeleteForm.tsx
+++ src/app/profil/components/form/DeleteForm.tsx
@@ -40,7 +40,6 @@ export default function DeleteForm({ user }: { user: User }) {
       try {
         await deleteAccount(id)
         showToast('Compte supprimé avec succès', 'success')
-        // await logout()
         setTimeout(() => router.push('/'), 1000)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
       } catch (err) {
