e417af41afdf11c2da4f991452fb2ff8fee714f9
Léa <lea.ballester@epitech.eu>
2026-07-03 13:21:16 +0200

    [FIX] Rajout de isLoggedIn pour vérifier la connexion de l'utilisateur ou non

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,18 @@
 import { useAuth } from './context/AuthContext'
 import PostList from './components/home/PostList'
 import AppLayout from './components/layout/AppLayout'
+import LandingPage from './components/home/LandingPage'
 
 export default function HomePage() {
-  const { isLoggedIn, user } = useAuth()
+  const { isLoggedIn, isAuthLoading, user } = useAuth()
+
+  if (isAuthLoading) {
+    return null
+  }
+
+  if (!isLoggedIn) {
+    return <LandingPage />
+  }
 
   return (
     <AppLayout sidebar>
